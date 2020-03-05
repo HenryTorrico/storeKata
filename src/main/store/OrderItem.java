5a951b5a6ef50903149a12a322ec3dc066fa8ce0
HenryTorrico <guillerbauti@gmail.com>
2020-03-04 23:33:23 -0400

    refactoring

--- src/main/store/OrderItem.java
+++ src/main/store/OrderItem.java
@@ -20,4 +20,65 @@ public class OrderItem {
 	public int getQuantity() {
 		return quantity;
 	}
+	
+	float calculateTotalForItem() {
+		float totalItem=0;
+		float discount = 0;
+		float itemAmount = getAmount(this);
+		if (isAccesory(this)) {
+			float booksDiscount = 0;
+			if (applyAccesoryDiscount(itemAmount)) {
+				booksDiscount = discountForBook(itemAmount);
+			}
+			totalItem = discountForCloth(itemAmount, booksDiscount);
+		}
+		if (isBike(this)) {
+			// 20% discount for Bikes
+			totalItem = discountForBike(itemAmount);
+		}
+		if (isCloth(this)) {
+			float cloathingDiscount = 0;
+			if (applyClothDiscount(this)) {
+				cloathingDiscount = this.getProduct().getUnitPrice();
+			}
+			totalItem = discountForCloth(itemAmount, cloathingDiscount);
+		}
+		return totalItem;
+	}
+	private float discountForCloth(float itemAmount, float cloathingDiscount) {
+		return itemAmount - cloathingDiscount;
+	}
+
+	private float discountForBike(float itemAmount) {
+		return itemAmount - itemAmount * 20 / 100;
+	}
+
+	private float discountForBook(float itemAmount) {
+		return itemAmount * 10 / 100;
+	}
+
+
+	private boolean applyClothDiscount(OrderItem item) {
+		return item.getQuantity() > 2;
+	}
+
+	private boolean isCloth(OrderItem item) {
+		return item.getProduct().getCategory() == ProductCategory.Cloathing;
+	}
+
+	private boolean isBike(OrderItem item) {
+		return item.getProduct().getCategory() == ProductCategory.Bikes;
+	}
+
+	private boolean applyAccesoryDiscount(float itemAmount) {
+		return itemAmount >= 100;
+	}
+
+	private boolean isAccesory(OrderItem item) {
+		return item.getProduct().getCategory() == ProductCategory.Accessories;
+	}
+	private float getAmount(OrderItem item) {
+		return item.getProduct().getUnitPrice() * item.getQuantity();
+	}
 }
+
